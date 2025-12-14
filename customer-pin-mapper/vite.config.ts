810e16efa877c082f4b9ab7caaae04e68dccb367
Nhongkham198 <zowamarketing@gmail.com>
2025-12-14 08:09:56 +0700

    rev.03

--- customer-pin-mapper/vite.config.ts
+++ customer-pin-mapper/vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
+    base: '/', 
     plugins: [react()],
     define: {
-      // Expose API_KEY to client-side code securely (ensure you only use this for public-safe keys or restrict usage)
+      // Expose API_KEY to client-side code securely
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
     },
     build: {
