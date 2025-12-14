cc656323e81576907230d023d13fd0ec610b0ef9
Nhongkham198 <zowamarketing@gmail.com>
2025-12-14 08:34:59 +0700

    rev.04

--- customer-pin-mapper/vite.config.ts
+++ customer-pin-mapper/vite.config.ts
@@ -3,17 +3,16 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
-    base: '/', 
+    base: '/',
     plugins: [react()],
     define: {
-      // Expose API_KEY to client-side code securely
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
     },
     build: {
       outDir: 'dist',
+      sourcemap: false
     }
   };
 });
